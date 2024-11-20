84e9fee9b302197d5d5c7596bd718661879ce752
Hromas <90126334+HromasDev@users.noreply.github.com>
2024-11-20 20:47:29 +0600

    Отключено проксирование сервера.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,7 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-const apiUrl = process.env.VITE_API_URL;
-
-console.log(apiUrl);
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: apiUrl,
-			},
-		},
-	},
+  plugins: [react()]
 })
